57c2030ea4f29e1184ec693ee36cbddd58bcbd1b
Venkata Naresh <divi.vnaresh@gmail.com>
2020-10-30 21:25:39 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,55 @@
+from caching import cacheForLRU
+from flask import Flask,request
+from bs4 import BeautifulSoup
+import sys,requests
+
+app = Flask(__name__)
+cache = cacheForLRU()
+
+def scrapeLink(url):
+    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36"
+    try:
+        response = requests.get(url, headers={'User-Agent': user_agent}, verify=False, timeout=(15, 20))
+        soup = BeautifulSoup(response.text,'html5lib')
+        return soup
+    except Exception:
+        print('Error on line {}'.format(sys.exc_info()[-1].tb_lineno))
+        return None
+
+def getStockCode(stockCode):
+    try:
+        soup = scrapeLink('https://finance.yahoo.com/quote/'+str(stockCode)+'/history?p='+str(stockCode))
+        if soup:
+            mainTable = soup.find('div',class_='Pb(10px) Ovx(a) W(100%)')
+            if mainTable: 
+                subTable = soup.find('table',class_='W(100%) M(0)').find('tbody').findAll('tr')[0].findAll('td')[-2]
+                if subTable: return subTable.find('span').text
+        return None
+    except Exception as e:
+        print('Error on line {}'.format(sys.exc_info()[-1].tb_lineno),e)
+        return None
+    
+def extractStockCurrentPrice(stockCode):
+    closingPrice = cache.get(stockCode)
+    if closingPrice!=-1:
+        return {'Source':'From Caching','stockCode':stockCode,'closingPrice':closingPrice}
+    else:
+        closingPrice = getStockCode(stockCode)
+        cache.put(stockCode, closingPrice)
+        return {'Source':'New Record','stockCode':stockCode,'closingPrice':closingPrice}
+
+@app.route('/')
+def start():
+    return 'API Successfully Created'
+
+@app.route('/getData',methods=['POST'])
+def processData():
+    if 'stockCode' in request.json:
+        stockCode = request.json["stockCode"]
+        data = extractStockCurrentPrice(stockCode)
+        return data
+    else:
+        return 'Please Provide Stock Code Details'
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0',port='7005')
---
+++ caching.py
@@ -0,0 +1,50 @@
+from collections import OrderedDict
+import time,sys,os.path
+
+class cacheForLRU:
+    def __init__(self):
+        self.cache = OrderedDict()
+        if os.path.isfile('backup.txt'):
+            self.cache = OrderedDict()
+            fr = open('backup.txt','r')
+            for line in fr:
+                if len(line.strip())>0:
+                    data = line.split('\t')
+                    self.cache[data[0]] = data[1]
+            fr.close()
+        self.maxSize,self.ttl,self.ttlCache = 5,30,{}
+        
+    def refreshCache(self):
+        try:
+            print('In Refresh Cache')
+            fw = open('backup.txt','w')
+            for key,val in self.cache.items():
+                fw.write(key+'\t'+val)
+                fw.write('\n')
+                fw.flush()
+                
+            current = time.time()
+            for key,val in self.ttlCache.items():
+                if current-val > self.ttl: del self.cache[key]
+            fw.close()
+        except Exception as e:
+            print('Error on line {}'.format(sys.exc_info()[-1].tb_lineno),e)
+    
+    def get(self,key):
+        try:
+            if key not in self.cache: return -1
+            else:
+                self.cache.move_to_end(key)
+                self.ttlCache[key] = time.time()
+                return self.cache[key]
+        except Exception as e:
+            print('Error on line {}'.format(sys.exc_info()[-1].tb_lineno),e)
+    
+    def put(self,key,val):
+        try:
+            self.cache[key],self.ttlCache[key] = val,time.time()
+            self.cache.move_to_end(key)
+            if len(self.cache)/self.maxSize >= 0.75: self.refreshCache()
+            if len(self.cache) > self.maxSize: self.cache.popitem(last=False)
+        except Exception as e:
+            print('Error on line {}'.format(sys.exc_info()[-1].tb_lineno),e)    
